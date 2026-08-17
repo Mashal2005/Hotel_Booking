5ba214f96515f9a495615c2f4822e0c518cb042f
Mashal2005 <abdomashal980@gmail.com>
2026-08-18 01:26:07 +0300

    change name

--- Models/Hotel.js
+++ Models/Hotel.js
@@ -1,9 +1,9 @@
 const mongoose=require('mongoose')
 
 const hotelSchema=new mongoose.Schema({
-    name:{
-        type:String,
-        required: true
+    name: {
+        type: String,
+        trim: true
     },
     address: {
         street: String,
